2dd31ea59dc9066486d2b4bcd430e3d9734eb4c6
João Malheiro <jonas_malheiro_sousa@hotmail.com>
2019-05-15 17:50:57 +0100

    server socket factory used

--- SDIS2/src/Peer.java
+++ SDIS2/src/Peer.java
@@ -1,5 +1,12 @@
-
-
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.SSLServerSocketFactory;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Peer {
 
@@ -7,15 +14,31 @@ public class Peer {
     private static String peerAcessPoint;
     private static String protocolVersion;
 
+    private static ServerSocket serverSocket;
+
     //private static Storage storage;
 
     public static void main(String args[]) throws IOException, ClassNotFoundException {
 
         initAtributes(args);
 
-        //RMIStub stub = null;
+        initializeServerSocket();
 
+        //RMIStub stub = null;
         Peer peer = new Peer();
+
+        String clientSentence;
+
+        while (true) {
+            Socket connectionSocket = serverSocket.accept();
+            BufferedReader inFromClient =
+                    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence = inFromClient.readLine();
+            System.out.println("Received: " + clientSentence);
+        }
+
+
         //stub = (RMIStub) UnicastRemoteObject.exportObject(peer, 0);
 
         /*try {
@@ -30,6 +53,7 @@ public class Peer {
             System.out.println("Peer connected through createRegistry");
         }
         */
+
     }
 
     private static void initAtributes(String[] args) throws IOException, ClassNotFoundException {
@@ -40,5 +64,18 @@ public class Peer {
         //storage = new Storage(1000000);
     }
 
+    private static void initializeServerSocket() {
+        int port = 0;
 
+        SSLServerSocketFactory serverSocketFactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+
+        try {
+            serverSocket = serverSocketFactory.createServerSocket(port);
+        } catch (IOException e) {
+            System.err.println("Error creating server socket");
+            e.printStackTrace();
+        }
+
+        //Thread th = new Thread(serverSocket);
+    }
 }
