20fa46343f5bcab0d69f3d46a36bd95b2dc15ede
João Malheiro <jonas_malheiro_sousa@hotmail.com>
2019-05-22 08:43:21 +0100

    try catch

--- SDIS2/src/PeerReceiver.java
+++ SDIS2/src/PeerReceiver.java
@@ -4,6 +4,7 @@ import javax.net.ssl.SSLSocket;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.UnknownHostException;
 
 
 public class PeerReceiver implements Runnable {
@@ -73,7 +74,11 @@ public class PeerReceiver implements Runnable {
             }
             
             System.out.println("Received: " + clientSentence);
-            Auxiliary.handleMessage(clientSentence);
+            try {
+                Auxiliary.handleMessage(clientSentence);
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
