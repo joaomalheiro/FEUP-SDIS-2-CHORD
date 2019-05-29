e2bbfb59b068d55bf09e4105a9df48a2acfe055b
João Malheiro <jonas_malheiro_sousa@hotmail.com>
2019-05-30 00:37:17 +0100

    Restore working, needs a LOT of testing

---
+++ SDIS2/src/messages/RestoreFile.java
@@ -0,0 +1,57 @@
+package messages;
+
+import chord.ConnectionInfo;
+import files.FileHandler;
+import peer.Peer;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.UnknownHostException;
+import java.util.concurrent.ExecutionException;
+
+public class RestoreFile extends Message {
+    private ConnectionInfo ci;
+    private BigInteger hashFile;
+    private String filename;
+    private byte[] body;
+    private String ipAddress;
+    private int port;
+
+    public RestoreFile(ConnectionInfo ci, BigInteger hashFile, String filename, byte[] body, String ipAddress, int port) {
+        this.ci = ci;
+        this.filename = filename;
+        this.hashFile = hashFile;
+        this.body = body;
+        this.ipAddress = ipAddress;
+        this.port = port;
+    }
+
+    @Override
+    public void handleMessage() throws UnknownHostException {
+
+        try {
+            FileHandler.writeFile("./peerDisk/peer" + Peer.getPeerAccessPoint() + "/restored/" + filename, body);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "RESTORE WITH FILE " + this.ci + " " + this.hashFile + " " + this.filename;
+    }
+
+    @Override
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    @Override
+    public int getPort() {
+        return port;
+    }
+}
---
+++ SDIS2/src/messages/RestoreInitMessage.java
@@ -0,0 +1,50 @@
+package messages;
+
+import chord.ChordInfo;
+import chord.ConnectionInfo;
+
+import java.math.BigInteger;
+import java.net.UnknownHostException;
+
+public class RestoreInitMessage extends Message {
+    private ConnectionInfo ci;
+    private BigInteger hashFile;
+    private String filename;
+    private String ipAddress;
+    private int port;
+
+    public RestoreInitMessage(ConnectionInfo ci, BigInteger hashFile,String filename, String ipAddress, int port) {
+        this.hashFile = hashFile;
+        this.filename = filename;
+        this.ci = ci;
+        this.ipAddress = ipAddress;
+        this.port = port;
+    }
+
+    @Override
+    public String toString() {
+        return "RESTORE-INIT" + this.ci + " " + this.hashFile + " " + this.filename;
+    }
+
+    @Override
+    public void handleMessage() throws UnknownHostException {
+
+        Message res = ChordInfo.searchSuccessor2(ci);
+        if(res instanceof SucessorMessage) {
+            MessageForwarder.sendMessage(new RestoreMessage(ci,this.hashFile, this.filename,res.getIpAddress(),res.getPort()));
+        } else if(res instanceof LookupMessage){
+            MessageForwarder.sendMessage(new RestoreInitMessage(ci,this.hashFile,this.filename, res.getIpAddress(),res.getPort()));
+        }
+
+    }
+
+    @Override
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    @Override
+    public int getPort() {
+        return port;
+    }
+}
